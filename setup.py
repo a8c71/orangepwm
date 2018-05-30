826e87c19b6f642e155aed5acf3c2a2a636fcec8
Fernando Mora <f.mora.g90@gmail.com>
2018-05-30 00:22:07 -0400

    install with pip

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='orangepwm',
+      version='0.1',
+      description='OPI PWM GPIO',
+      url='https://github.com/a8c71/orangepwm.git',
+      author='Fernando Mora',
+      author_email='f.mora.g90@gmail.com',
+      license='MIT',
+      packages=['orangepwm'],
+      zip_safe=False)
